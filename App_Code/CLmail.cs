69b0cf10b2286cff63344710a03f80f8f1f8eb4d
Eren Karaman <erenkaraman58@hotmail.com>
2020-07-20 10:21:29 +0300

    Update CLmail.cs

--- App_Code/CLmail.cs
+++ App_Code/CLmail.cs
@@ -6,7 +6,7 @@ using System.Web;
 public class CLmail
 {
     public string mailAdresi = "etmdemo@webdevtasarim.com";
-    public string mailSifresi = "Asd123456789***";
+    public string mailSifresi = "";
     public string nasilGozuksun = "Etm.webdevtasarim.com";
     public int port = 587;
     public int port2 = 143;//mailkit
